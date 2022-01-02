ea96d0c1aed69c74461aef2346fc0b88cecb5223
dilapitan <domlapitan@gmail.com>
2022-01-02 13:37:36 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <br /><br />
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="3" sm="2" md="1">
+        <v-subheader class="text-body-1">Start</v-subheader>
+      </v-col>
+      <v-col cols="3" sm="2" md="1">
+        <v-text-field
+          v-model="inputStartPage"
+          type="number"
+          outlined
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3" sm="2" md="1">
+        <v-subheader class="text-body-1">Daily Pages</v-subheader>
+      </v-col>
+      <v-col cols="3" sm="2" md="1">
+        <v-text-field
+          v-model="inputDailyPages"
+          type="number"
+          outlined
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  watch: {
+    inputStartPage: function () {
+      if (this.inputStartPage < 0) {
+        this.inputStartPage = Math.abs(this.inputStartPage)
+      }
+    },
+
+    inputDailyPages: function () {
+      if (this.inputDailyPages < 0) {
+        this.inputDailyPages = Math.abs(this.inputDailyPages)
+      }
+    }
+  },
+
+  data: () => ({
+    inputStartPage: null,
+    inputDailyPages: null
+  })
+}
+</script>
